efd795cc8da9b58d8b93de98d54e489ebef2a8fa
Armen Rostamian <arostamian@ulive.com>
2016-12-12 11:33:13 -0800

    yay clean setup

---
+++ Exercises.js
@@ -0,0 +1,40 @@
+/*
+ Given the following array of objects, answer the following questions with two solution sets.
+ Your first solution should utilize imperative programming techniques.
+ Your second solution should utilize functional programming paradigms.
+*/
+products = [
+    { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+    { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+    { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+    { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+    { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+];
+
+// You're allergic to nuts and mushrooms make you squeamish. Find only those pizzas which you will enjoy.
+
+
+// Add all natural numbers below 1000 that are multiples of 3 or 5
+
+
+// Count the # of occurrences of ingredients
+
+
+
+/*
+From here on out, assume that the expected solution is a function
+*/
+
+// Write a function that finds the largest prime factor of a composite number
+
+
+// Find the largest palindrome made from the product of two 3 digit numbers
+
+
+// Find the smallest number divisible by each of the numbers 1 to 20
+
+
+// Find the difference between the sum of the squares and the square of the sums
+
+
+// Find the 10001st prime
---
+++ README.md
@@ -0,0 +1,9 @@
+## Javascript Practice Problems
+
+These come from the "AboutApplyingWhatWeHaveLearnt" JS KOANS practice set. 
+
+I simply copied out the questions, excising the included code for scaffolding and test conditions. 
+
+All credit and thanks go to the original author of the JavaScript Koans in this repo: 
+
+https://github.com/mrdavidlaing/javascript-koans
